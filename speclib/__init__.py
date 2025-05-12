698f51178be31b3f73cd575919342eddd9423ce4
brackham <brackham@mit.edu>
2025-05-12 17:11:41 -0400

    🔥 Remove `_astropy_init.py`

--- speclib/__init__.py
+++ speclib/__init__.py
@@ -3,8 +3,6 @@
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import *  # noqa
-
 import os
 
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
