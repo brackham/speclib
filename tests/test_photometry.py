17d66235903182c8e85f8762572483460b274cd5
brackham <brackham@mit.edu>
2025-05-30 16:32:09 -0400

    :art: Black formatting

--- tests/test_photometry.py
+++ tests/test_photometry.py
@@ -2,10 +2,12 @@ from speclib import Spectrum, Filter, apply_filter
 import numpy as np
 import astropy.units as u
 
+
 def test_filter_initialization():
     f = Filter("2MASS J")
     assert f.name == "2MASS J"
     assert hasattr(f, "response")
+
 
 def test_apply_filter_succeeds():
     wave = np.linspace(11000, 13500, 100) * u.AA
@@ -14,4 +16,6 @@ def test_apply_filter_succeeds():
     filt = Filter("2MASS J")
 
     result = apply_filter(spec, filt)
-    assert result.unit.is_equivalent(u.erg / u.s / u.cm**2 / u.AA)  # might change later
+    assert result.unit.is_equivalent(
+        u.erg / u.s / u.cm**2 / u.AA
+    )  # might change later
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 from speclib.utils import nearest
 
+
 def test_nearest_simple():
     arr = [10, 20, 30]
     val = 19
